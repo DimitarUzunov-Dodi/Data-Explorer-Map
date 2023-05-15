b48946ca39c2ab003e2f95a4091be61154f6beb1
Dafni <d.v.pandeva@student.tudelft.nl>
2023-05-15 23:08:53 +0200

    additional changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular';
-
-  
 }
 
 
