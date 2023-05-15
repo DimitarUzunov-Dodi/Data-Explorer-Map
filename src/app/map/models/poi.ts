9a2970cd7c6c2aff5cced18e3eb904e8a74f7d9d
Atanas <A.B.Kichukov@student.tudelft.nl>
2023-05-15 22:59:50 +0200

    changed poi type to enum

--- src/app/map/models/poi.ts
+++ src/app/map/models/poi.ts
@@ -1,15 +1,26 @@
-class PointOfInterest {
-  type: 'fog' | 'icy road' | 'strong wind' | 'pothole' | 'police' | 'road hazard';
+export class PointOfInterest {
+  type: RoadHazardType;
   date: Date;
   hexId: string;
   isActive: boolean;
 
-  constructor(type: 'fog' | 'icy road' | 'strong wind' | 'pothole' | 'police' | 'road hazard', date: Date, hexId: string, isActive: boolean) {
+  constructor(type: RoadHazardType, date: Date, hexId: string, isActive: boolean) {
     this.type = type;
     this.date = date;
     this.hexId = hexId;
     this.isActive = isActive;
   }
+}
 
-
+export enum RoadHazardType {
+  Potholes = 'Potholes',
+  Fog = 'Fog',
+  Aquaplaning = 'Aquaplaning',
+  IcyRoads = 'Icy Roads',
+  TrafficJams = 'Traffic Jams',
+  RoadEmergencies = 'Road Emergencies',
+  RoadConditions = 'Road Conditions',
+  Police = 'Police',
+  CamerasAndRadars = 'Cameras And Radars',
+  Incidents = 'Incidents',
 }
