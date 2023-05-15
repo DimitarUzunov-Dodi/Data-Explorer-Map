209df8367ea11ce2434a433a6c774c5ee5ac5c8b
Atanas <A.B.Kichukov@student.tudelft.nl>
2023-05-15 23:00:33 +0200

    created a method to append the checkboxes on page load

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PointOfInterest, RoadHazardType } from '../map/models/poi'
 import { isValidCell } from 'h3-js';
 
 @Component({
@@ -8,7 +9,36 @@ import { isValidCell } from 'h3-js';
   })
 
 export class FilterCheckbox {
+    ngOnInit(): void {
+        this.initializeCheckBox();
+      }
+
+    initializeCheckBox() {
+
+        // add try/catch to make sure poi is array of string and form is an HTMLElement ?
+        const poi : RoadHazardType[] = Object.values(RoadHazardType);
+        const form : HTMLElement | null = document.getElementById('checkBoxForm');
+
+        // console.log(poi);
+
+        for (var i : number = 0; i < poi.length; i++) {
+            var haz : string = poi[i].toString();
+
+            const checkBox : HTMLInputElement = document.createElement('input');
+            checkBox.type = 'checkbox';
+            //checkBox.checked = true
+            checkBox.value = haz;
+
+            const label : HTMLLabelElement = document.createElement('label');
+            label.textContent = haz;
+
+            form?.appendChild(checkBox);
+            form?.appendChild(label);
+            form?.appendChild(document.createElement('br'));
+        }
+    }
+
     // handleCheckboxChange() {
-        
+
     // }
 }
