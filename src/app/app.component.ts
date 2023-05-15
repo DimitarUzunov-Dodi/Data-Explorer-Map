75a61d2903f8ed5ace12f4714c6f2bc5fb133639
Dafni <d.v.pandeva@student.tudelft.nl>
2023-05-15 14:02:44 +0200

    cleaned up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular';
+
+  ngOnInit() {
+    const infotainmentPanel = document.querySelector('.infotainment-panel');
+    const togglePanelBtn = document.querySelector('#toggle-panel-btn');
+    if(togglePanelBtn!=null && infotainmentPanel!=null){
+      togglePanelBtn.addEventListener('click', () => {
+        infotainmentPanel.classList.toggle('show');
+      });
+    }
+    
+  }
 }
+
+
