afe6f08dfdb58c70c5e9a8eb33a634df3527a8ba
Dafni <d.v.pandeva@student.tudelft.nl>
2023-05-16 13:11:13 +0200

    fix pipeline

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { InfotainmentPanelComponent } from './infotainment-panel/infotainment-panel.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [
-        AppComponent
+        AppComponent,
+        InfotainmentPanelComponent
       ],
     }).compileComponents();
   });
