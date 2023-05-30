bea967bdd7ffd9aea464e348d15fb4bdfb3fc8b6
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2023-05-30 23:18:37 +0200

    pipe fix

--- src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.spec.ts
+++ src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { HexagonInfotainmentPanelComponent } from './hexagon-infotainment-panel.component';
 
@@ -8,7 +9,8 @@ describe('HexagonInfotainmentPanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HexagonInfotainmentPanelComponent ]
+      declarations: [ HexagonInfotainmentPanelComponent ],
+      imports: [HttpClientTestingModule], 
     })
     .compileComponents();
 
