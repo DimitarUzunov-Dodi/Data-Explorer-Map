7f45c686ad189766009a66b9f24924dd54428e97
Dafni <d.v.pandeva@student.tudelft.nl>
2023-05-30 16:08:01 +0200

    display info in panel

--- src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.ts
+++ src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.ts
@@ -9,14 +9,34 @@ import * as h3 from 'h3-js';
 export class HexagonInfotainmentPanelComponent {
   @Input() showInfotainmentPanel: boolean = false;
   @Input() searchedHex: string = '';
+  parentHexId: string = '';
+
 
   hexagonInfotainmentPanel: HexagonInfotainmentPanelComponent | null = null;
   area: number = 0;
 
+  calculateParentHexId(): void {
+    const resoulution = h3.getResolution(this.searchedHex);
+    if(resoulution!=-1){
+      this.parentHexId=  h3.cellToParent(this.searchedHex, resoulution-1);
+    }
+  }
+
+  calculateArea():void{
+    const areaMeters = (h3.cellArea(this.searchedHex, 'km2')).toFixed(3);
+    this.area =+areaMeters;
+
+  }
+
+  ngOnInit(): void {
+    this.calculateParentHexId();
+    this.calculateArea();
+  }
+
   ngOnChanges(): void {
     if (h3.isValidCell(this.searchedHex)) {
       this.hexagonInfotainmentPanel = this;
-      this.area = h3.cellArea(this.searchedHex, 'km2');
+     // this.area = h3.cellArea(this.searchedHex, 'km2');
     } else {
       this.hexagonInfotainmentPanel = null;
     }
