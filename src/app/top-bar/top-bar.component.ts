1c9fec7d1a3a847fc718dd28fd24421eeb238ab8
Dimitar Uzunov <D.S.Uzunov-1@student.tudelft.nl>
2023-05-30 17:12:08 +0200

    Change of  placeholder text to switch between searches

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -8,10 +8,18 @@ import { Component, Output, EventEmitter } from '@angular/core';
 })
 export class TopBarComponent {
   searchText: string = '';
+  searchBar: string = 'Search by Hex';
   @Output() searchTriggered: EventEmitter<string> = new EventEmitter<string>();
   @Output() clearSearchTriggered: EventEmitter<any> = new EventEmitter<any>();
   triggerSearch() {
     this.searchTriggered.emit(this.searchText); 
+  }
+  switchSearch() {
+    if(this.searchBar === 'Search by Hex'){
+      this.searchBar = 'Search by POI';
+    }else{
+      this.searchBar = 'Search by Hex';
+    }
   }
   triggerClearSearch() {
     this.clearSearchTriggered.emit(); 
