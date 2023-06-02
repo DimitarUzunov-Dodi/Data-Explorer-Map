2f5ed0b4799b8e4f780948fd8159b5f7f619eb89
Atanas <A.B.Kichukov@student.tudelft.nl>
2023-06-02 16:24:39 +0300

    reformat and quality of life

---
+++ src/app/Services/models/mapModels.ts
@@ -0,0 +1,9 @@
+export enum ResolutionLevel {
+    CountryLevel = 1,
+    StateLevel = 3,
+    CityLevel = 5,
+    TownLevel = 7,
+    HighWayLevel = 9,
+    RoadLevel = 11,
+    RoadwayLevel = 13
+  }
