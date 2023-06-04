3bd36662c38b748fbb44d7a30d7956da4ce34ef0
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2023-06-04 21:06:13 +0300

    fix pipe

--- src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.spec.ts
+++ src/app/infotainment-panel/hexagon-infotainment-panel/hexagon-infotainment-panel.component.spec.ts
@@ -1,9 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PieChartComponent} from "src/app/pie-chart/pie-chart.component";
-
-
 import { HexagonInfotainmentPanelComponent } from './hexagon-infotainment-panel.component';
+import { HomepageComponent } from 'src/app/homepage/homepage.component';
 
 describe('HexagonInfotainmentPanelComponent', () => {
   let component: HexagonInfotainmentPanelComponent;
@@ -13,6 +12,7 @@ describe('HexagonInfotainmentPanelComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ HexagonInfotainmentPanelComponent, PieChartComponent ],
       imports: [HttpClientTestingModule],
+      providers: [HomepageComponent], 
     })
     .compileComponents();
 
