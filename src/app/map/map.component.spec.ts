2535ffbf555fdd5b33e47921e2ae345a7d0b9deb
Dafni <d.v.pandeva@student.tudelft.nl>
2023-06-05 03:17:07 +0200

    fix pipeline

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MapComponent } from './map.component';
+import { HomepageComponent } from '../homepage/homepage.component';
 
 describe('MapComponent', () => {
   let component: MapComponent;
@@ -8,7 +9,8 @@ describe('MapComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ MapComponent ],
+      providers: [HomepageComponent]
     })
     .compileComponents();
 
