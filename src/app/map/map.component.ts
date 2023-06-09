cc16d0de6f250a4073bdd569dd19ec4e9c8aa344
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2023-06-09 12:01:55 +0300

    removed unused method

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -454,12 +454,6 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.searchedHazards = neededHazards;
   }
 
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null){
-     this.center = (event.latLng.toJSON());
-    }
-  }
-
   search(searchTuple: [string,string]): void {
     this.clearSearch()
     switch (searchTuple[0]) {
