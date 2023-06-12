346d4851aa4923ae1f2f64be8c4bb63e5f08cac1
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2023-06-12 15:13:04 +0300

    fix

--- src/app/infotainment-panel/region-infotainment-panel/region-infotainment-panel.component.spec.ts
+++ src/app/infotainment-panel/region-infotainment-panel/region-infotainment-panel.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegionInfotainmentPanelComponent } from './region-infotainment-panel.component';
 import { MapComponent } from 'src/app/map/map.component';
+
 describe('RegionInfotainmentPanelComponent', () => {
   let component: RegionInfotainmentPanelComponent;
   let fixture: ComponentFixture<RegionInfotainmentPanelComponent>;
@@ -8,7 +9,7 @@ describe('RegionInfotainmentPanelComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RegionInfotainmentPanelComponent],
-      providers: [MapComponent]
+      providers: [MapComponent],
     })
     .compileComponents();
 
