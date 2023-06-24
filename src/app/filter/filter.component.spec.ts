27bb8c70db4af63a0ffce33b84d033506b604f86
Ivan Virovski <i.virovski-1@student.tudelft.nl>
2023-06-24 15:23:40 +0200

    update to tests

--- src/app/filter/filter.component.spec.ts
+++ src/app/filter/filter.component.spec.ts
@@ -40,20 +40,68 @@ describe('FilterCheckbox', () => {
 
   it('should emit a signal to indicate all hazards selection and update checkbox states', () => {
     spyOn(component.shouldSelectAll, 'emit');
-  
-    component.selectAll(false);
-    expect(component.shouldSelectAll.emit).toHaveBeenCalledWith(false);
-  
-    component.selectAll(true);
-    expect(component.shouldSelectAll.emit).toHaveBeenCalledWith(true);
+    const boxes: HTMLInputElement[] = fixture.nativeElement.querySelectorAll('input[name="boxcheck"]');
+    const areChecked = true;
+
+    component.selectAll(areChecked);
+
+    expect(component.shouldSelectAll.emit).toHaveBeenCalledWith(areChecked);
+
+    for (const box of boxes) {
+      expect(box.checked).toBe(areChecked);
+    }
   });
-  
+
+  it('should emit a signal to indicate all hazards deselection and update checkbox states', () => {
+    spyOn(component.shouldSelectAll, 'emit');
+    const boxes: HTMLInputElement[] = fixture.nativeElement.querySelectorAll('input[name="boxcheck"]');
+    const areChecked = false;
+    component.selectAll(areChecked);
+    expect(component.shouldSelectAll.emit).toHaveBeenCalledWith(areChecked);
+
+    for (const box of boxes) {
+      expect(box.checked).toBe(areChecked);
+    }
+  });
+
+  it('should handle error gracefully in selectAll() when document.getElementsByName() fails', () => {
+    spyOn(console, 'log');
+    spyOn(component.shouldSelectAll, 'emit');
+    spyOn(document, 'getElementsByName').and.throwError('Error occurred');
+
+    component.selectAll(true);
+
+    expect(console.log).toHaveBeenCalled();
+    expect(component.shouldSelectAll.emit).not.toHaveBeenCalled();
+  });
 
   it('should emit hazardChecked event when handleCheckboxChange() is called', () => {
     spyOn(component.hazardChecked, 'emit');
     const hazardType = Object.keys(RoadHazardType)[0];
     const isChecked = true;
+
     component.handleCheckboxChange(hazardType, isChecked);
+
     expect(component.hazardChecked.emit).toHaveBeenCalledWith([hazardType, isChecked]);
+  });
+
+  it('should handle ngOnInit() to initialize hazards list correctly', () => {
+    const hazardValues: string[] = Object.values(RoadHazardType);
+    component.ngOnInit();
+
+    expect(component.hazards.length).toBe(hazardValues.length);
+
+    for (let i = 0; i < hazardValues.length; i++) {
+      expect(component.hazards[i].value).toBe(hazardValues[i]);
+      expect(component.hazards[i].checked).toBeTruthy();
+    }
+  });
+
+  it('should handle edge case when there are no hazards in ngOnInit()', () => {
+    spyOn(Object, 'values').and.returnValue([]);
+
+    component.ngOnInit();
+
+    expect(component.hazards.length).toBe(0);
   });
 });
